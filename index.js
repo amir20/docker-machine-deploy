758ef41eff88cc530ecbb0ce4c269c051be2abde
Amir Raminfar <findamir@gmail.com>
2017-08-16 08:45:36 -0700

    Adds debug and some logging with reading json file for config

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+#!/usr/bin/env node
+
 const ora = require("ora");
 const { spawn, exec } = require("child-process-promise");
+const debug = require("debug")("docker-machine-deploy");
+const { promisify } = require("util");
+const path = require("path");
+const fs = require("fs");
+const readFileAsync = promisify(fs.readFile);
 
-async function parseEnvVars() {
-  const envOutput = await exec("docker-machine env --shell bash clash");
+async function parseConfig() {
+  const json = await readFileAsync(path.resolve(process.cwd(), ".docker-machine-deploy.json"), "utf8");
+  return JSON.parse(json);
+}
+
+async function parseDockerEnv({ machine }) {
+  const { stdout } = await exec(`docker-machine env --shell bash ${machine}`);
   const re = /export (\w+)="(.+?)"/gm;
 
   const env = {};
-  for (let match; (match = re.exec(envOutput.stdout)); ) {
-    const [_, name, value] = match;
+  for (let match; (match = re.exec(stdout)); ) {
+    const [, name, value] = match;
     env[name] = value;
   }
 
@@ -15,17 +27,23 @@ async function parseEnvVars() {
 }
 
 (async function() {
+  const spinner = ora();
   try {
-    const spinner = ora(`Reading up environment variables for [clash] machine...`).start();
-    const env = await parseEnvVars();
+    spinner.start();
+    spinner.text = "Parsing config file...";
+    const config = await parseConfig();
+    debug("Found config file %O", config);
+
+    spinner.text = `Reading env variables for [${config.machine}]...`;
+    const env = await parseDockerEnv(config);
+    debug("%O", env);
 
     spinner.text = "Executing ps...";
     const test = await exec("docker ps", { env });
 
-    console.log(test.stdout);
-
     spinner.succeed("Done");
   } catch (error) {
+    spinner.fail(error.message);
     console.error(error);
   }
 })();
